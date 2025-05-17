9d68b9409f62cfb4b382b2220907eb6ce7fc059e
Earlinn Drow <earlinndrow@gmail.com>
2025-05-17 16:34:51 +0500

    Show username before chat message

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -26,18 +26,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        username = self.scope["user"].username
 
         # отправляем сообщение в группу
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
-                "message": message
+                "message": message,
+                "username": username,
             }
         )
 
     async def chat_message(self, event):
         message = event["message"]
+        username = event["username"]
 
         # отправляем сообщение обратно клиенту
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(
+            text_data=json.dumps({"message": message, "username": username})
+        )
